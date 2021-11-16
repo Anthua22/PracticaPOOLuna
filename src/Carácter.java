efbd5ce7bb38989b167db1331274ab7af49ee26c
anthua22 <anthonyemmanuel.ubillus@iesdoctorbalmis.com>
2021-11-17 00:17:39 +0100

    clase caracter hecha

--- src/Carácter.java
+++ src/Carácter.java
@@ -8,4 +8,31 @@
  */
 public class Carácter {
 
+	private static char[] vocales = { 'A', 'E', 'I', 'O', 'U' };
+	private static char[] consonantes = { 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'P', 'Q', 'R',
+			'S', 'T', 'V', 'X', 'Z', 'W', 'Y' };
+
+	public static boolean esVocal(char letra) {
+		String letraModificada = (letra + "").toUpperCase();
+		char letraMayuscula = letraModificada.toCharArray()[0];
+		boolean encontrada = false;
+		for (int i = 0; i < vocales.length; i++) {
+			if (vocales[i] == letraMayuscula) {
+				encontrada = true;
+			}
+		}
+		return encontrada;
+	}
+
+	public static boolean esConsonante(char consonante) {
+		String letraModificada = (consonante + "").toLowerCase();
+		char letraMayuscula = letraModificada.toCharArray()[0];
+		boolean encontrada = false;
+		for (int i = 0; i < consonantes.length; i++) {
+			if (consonantes[i] == letraMayuscula) {
+				encontrada = true;
+			}
+		}
+		return encontrada;
+	}
 }
