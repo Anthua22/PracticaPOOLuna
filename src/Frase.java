37f9cd7afab0ec1578e6bd59a084d5701a15ead1
anthua22 <anthonyemmanuel.ubillus@iesdoctorbalmis.com>
2021-11-16 23:59:32 +0100

    clase frase hecha falta rellenar el array con las frases

--- src/Frase.java
+++ src/Frase.java
@@ -1,9 +1,14 @@
+import java.util.Random;
 
 public class Frase {
 
+	/// Añadir mas frases siguiendo la coma y después la cadena
+	private static String[] frasesJuego = {"Invierno","ddd"};
 	
-	public static void dameFrase() {
-		
+	public static String dameFrase() {
+		Random aletorio = new Random();
+		int posicionAleatoria = aletorio.nextInt(4);
+		return frasesJuego[posicionAleatoria];
 	}
 	
 }
